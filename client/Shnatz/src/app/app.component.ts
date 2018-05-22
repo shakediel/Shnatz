1cdd2ce10b49159e76e5697b0eb308f87c674a05
Shakediel Hiba <shaked921@gmail.com>
2018-05-22 23:31:23 +0300

    authentication with google

--- client/Shnatz/src/app/app.component.ts
+++ client/Shnatz/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(public afAuth: AngularFireAuth) {
+    let config = {
+      apiKey: "AIzaSyBZw6VyDQGvU_W5Yl1le757uOkXditWOtU",
+      authDomain: "trytry-b24c8.firebaseapp.com",
+      databaseURL: "https://trytry-b24c8.firebaseio.com",
+      projectId: "trytry-b24c8",
+      storageBucket: "trytry-b24c8.appspot.com",
+      messagingSenderId: "879603139713"
+    };
+    firebase.initializeApp(config);
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
+
+
+
