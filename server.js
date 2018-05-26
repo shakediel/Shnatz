0157781b454aa89f178ef68e48304588561a94c9
Shaked Hiba <shaked921@gmail.com>
2018-05-26 21:56:38 +0300

    updated server

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 // server.js
 const express = require('express');
+var path = require("path");
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Shnatz'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Shnatz/index.html'));
 });
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function () {
+  console.log('client is up!');
+});
